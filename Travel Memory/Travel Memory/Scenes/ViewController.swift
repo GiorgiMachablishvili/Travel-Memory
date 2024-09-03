8533e3abd0b3d4802fa38ac2d5d3416ceb64eaa1
GiorgiMachablishvili <g.machablishvili@yahoo.com>
2024-09-03 11:27:14 +0400

    delete story board, create folders and add files in folder

---
+++ Travel Memory/Travel Memory/Scenes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Travel Memory
+//
+//  Created by Gio's Mac on 03.09.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
